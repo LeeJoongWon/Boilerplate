9216c2ab77e2cb94bb467ff98510e1c44e330ccb
lee <dlwnddnjs48@gmail.com>
2022-04-04 16:28:46 +0900

    monggooseDB연결함

--- Login/index.js
+++ Login/index.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const app = express()
 const port = 5000
-const uri = "mongodb+srv://JoongWon:1234@boilerplate-login.frbz2.mongodb.net/Boilerplate-Login?retryWrites=true&w=majority";
 
+const { User } = require("./models/User");
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+const uri = "mongodb+srv://JoongWon:1234@boilerplate-login.frbz2.mongodb.net/Boilerplate-Login?retryWrites=true&w=majority";
 const mongoose = require('mongoose')
 mongoose.connect(uri)
  .then(() => console.log('MongoDB Connected...'))
@@ -11,6 +16,18 @@ mongoose.connect(uri)
 
 app.get('/', (req, res) => {
   res.send('Hello World! 안녕asd')
+})
+
+app.post('/register', (req,res) => {
+
+  const user = new User(req.body)
+
+  user.save((err,userInfo) =>{
+    if(err) return res.json({ success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
 })
 
 app.listen(port, () => {
